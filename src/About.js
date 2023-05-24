47bcddf2faf85972257c4bd0f41ebd0cb8096111
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-05-23 21:49:49 -0700

    fixed errors

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 
 class Profile extends Component {
@@ -6,9 +6,7 @@ class Profile extends Component {
   render() {
     /* TODO: render information about the developers */
     return <>
-      
-      <p>Hi, my name is Josh and I am a full stack software engineer specializing in Javascript. I am a military veteran  with a decade of experience in aerospace, and currently work in administration management. Software is an exciting industry to me because of the unlimited creative potential and I still believethere is a whole world of untapped potential in this industry. What gets me out of bed in the morning is working hard and having fun doing it. I have a strong desire to be a part of a motivated, collaborative team that encourages learning and personal development .  I feel a deep satisfaction when I complete a task and will to bring my passion and drive to the workplace.
-      </p>;
+      <p>Hi, my name is Josh and I am a full stack software engineer specializing in Javascript. I am a military veteran  with a decade of experience in aerospace, and currently work in administration management. Software is an exciting industry to me because of the unlimited creative potential and I still believethere is a whole world of untapped potential in this industry. What gets me out of bed in the morning is working hard and having fun doing it. I have a strong desire to be a part of a motivated, collaborative team that encourages learning and personal development .  I feel a deep satisfaction when I complete a task and will to bring my passion and drive to the workplace.</p>;
 
       <h1>My name is Emily Greason, I am the best software engineer ever.</h1>
     </>;
